a8b59b684f20a913a7913b4d327c5c5b1ff950cd
He xinying <22447350@life.hkbu.edu.hk>
2023-02-18 10:10:15 +0800

    updates for output info

--- clean.py
+++ clean.py
@@ -4,7 +4,7 @@ import pandas as pd
 def clean(input_file1,input_file2):
     df1= pd.read_csv(input_file1)
     df2=pd.read_csv(input_file2)
-    df=pd.merge(df1,df2,left_on='respondent_id',right_on="id",how="left")
+    df=pd.merge(df1,df2,left_on='respondent_id',right_on="id")
     df = df.dropna()
     df = df[df["job"].str.contains("insurance|Insurance") == False]
     df = df.drop(["id"], axis=1)
